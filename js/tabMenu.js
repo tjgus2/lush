9a4b832f6cbdbab1e47a9fb6d37d90ec4c5d86ed
tjgus2 <162402315+tjgus2@users.noreply.github.com>
2025-07-01 15:10:11 +0900

    6

---
+++ js/tabMenu.js
@@ -0,0 +1,12 @@
+const tabButtons = document.querySelectorAll(".tab-btn");
+const tabContents = document.querySelectorAll(".tab-content");
+
+tabButtons.forEach(btn => {
+    btn.addEventListener("click", () => {
+        tabButtons.forEach(b => b.classList.remove('active'));
+        tabContents.forEach(c => c.classList.remove('active'));
+
+        btn.classList.add('active');
+        document.getElementById(btn.dataset.tab).classList.add('active');
+    })
+})
