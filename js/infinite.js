fdab133f5510ad9d5fce44d357280387b0b0c8cf
tjgus2 <162402315+tjgus2@users.noreply.github.com>
2025-07-11 23:10:53 +0900

    10

--- js/infinite.js
+++ js/infinite.js
@@ -1,22 +1,21 @@
-(function () {
-    const infiniteSlider = document.getElementById('infiniteSlider');
-    const sliderContent = document.getElementById('sliderContent');
-  
-    const clone = sliderContent.cloneNode(true);
-    infiniteSlider.appendChild(clone);
-  
-    let position = 0;
-    const speed = 1;
-  
-    function animate() {
-      position -= speed;
-      if (Math.abs(position) >= sliderContent.offsetWidth) {
-        position = 0;
-      }
-      infiniteSlider.style.transform = `translateX(${position}px)`;
-      requestAnimationFrame(animate);
+window.addEventListener('load', () => {
+  const infiniteSlider = document.getElementById('infiniteSlider');
+  const sliderContent = document.getElementById('sliderContent');
+
+  const clone = sliderContent.cloneNode(true);
+  infiniteSlider.appendChild(clone);
+
+  let position = 0;
+  const speed = 1;
+
+  function animate() {
+    position -= speed;
+    if (Math.abs(position) >= sliderContent.offsetWidth) {
+      position = 0;
     }
-  
-    animate();
-  })();
-  
+    infiniteSlider.style.transform = `translateX(${position}px)`;
+    requestAnimationFrame(animate);
+  }
+
+  animate();
+});
